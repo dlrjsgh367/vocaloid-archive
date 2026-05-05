c570123dc71db6b67bf9a6b7196e7cb0ee6b2065
이건호 <dlrjsgh367@gmail.com>
2026-05-06 05:35:17 +0900

    feat(like): add Like entity mapping for Phase 3 sort=popular

---
+++ backend/src/main/java/com/vocaloidarchive/like/domain/LikeId.java
@@ -0,0 +1,29 @@
+package com.vocaloidarchive.like.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class LikeId implements Serializable {
+
+  private Long user;
+  private Long song;
+
+  public LikeId() {}
+
+  public LikeId(Long user, Long song) {
+    this.user = user;
+    this.song = song;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof LikeId that)) return false;
+    return Objects.equals(user, that.user) && Objects.equals(song, that.song);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(user, song);
+  }
+}
